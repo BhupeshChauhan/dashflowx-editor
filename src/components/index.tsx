15ba62663089850a178bd784cb41e328fc5292e1
BhupeshChauhan <erchauhanbhupesh@gmail.com>
2024-06-08 12:26:26 +0530

    export correction

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,5 @@
 export { Editor } from './Editor';
-export { EditorProvider } from '../providers/editor-provider';
+export { EditorProvider, useEditor } from '../providers/editor-provider';
 export { EditorNavigation } from '../components/EditorNavigation';
 export { EditorSidebar } from '../components/EditorSidebar';
 export { EditorLiveView } from '../components/LiveView';
